e0ee37ada7dc97c0552d8e80f0ccbb61ee2b48ac
farzanalware <farzanalware46@gmail.com>
2026-08-17 15:08:18 +0530

    Add files via upload

--- exam-score.py
+++ exam-score.py
@@ -14,3 +14,18 @@ plt.legend()
 plt.show()
 
 
+#----------------------------#
+
+import matplotlib.pyplot as plt 
+import numpy as np
+# Line Plot
+x = np.linspace(0, 10, 100)
+plt.plot(x, np.sin(x), label="sin(x)")
+plt.plot(x, np.cos(x), label="cos(x)")
+plt.title("Trigonometric Functions")
+plt.xlabel("x")
+plt.ylabel("y")
+
+plt.legend()
+plt.grid(True)
+plt.show()
